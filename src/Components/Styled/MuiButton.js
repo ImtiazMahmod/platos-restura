256ca95ea5c0c950229b12fc00a988c628ce8c32
ImtiazMahmod <imtiazmahmod2@gmail.com>
2022-04-19 11:25:06 +0600

    styled

---
+++ src/Components/Styled/MuiButton.js
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+import { Button } from "@mui/material";
+
+
+const MuiButton = styled(Button)({
+  color: "white",
+  background: "linear-gradient(90deg,tomato,orange)",
+  padding: "10px 35px",
+  fontWeight: 500,
+});
+
+export default MuiButton;
