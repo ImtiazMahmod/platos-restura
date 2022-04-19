415680b9d521adba232b31bd4d802d25cdbbdfc5
ImtiazMahmod <imtiazmahmod2@gmail.com>
2022-04-19 11:23:52 +0600

    notfound

---
+++ src/Pages/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div style={{backgroundColor:"black",height:"100vh",overflow:"hidden"}}>
+          <img style={{backgroundSize: "cover"}} width={"100%" } height="100%" src="https://freefrontend.com/assets/img/html-funny-404-pages/HTML-404-Error-Page.gif" alt="" />
+            
+        </div>
+    );
+};
+
+export default NotFound;
